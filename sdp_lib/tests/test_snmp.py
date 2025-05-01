e6f294e6baa4397d7a1e7e7d7b58fef816f70a39
auser <unnmd88@gmail.com>
2025-05-01 20:46:06 +0300

    Добавлен тест

--- sdp_lib/tests/test_snmp.py
+++ sdp_lib/tests/test_snmp.py
@@ -1,8 +1,22 @@
 import asyncio
+from collections import abc
 
+import pytest
 from pysnmp.entity.engine import SnmpEngine
 
 from sdp_lib.management_controllers.snmp import snmp_api
+
+
+pytest_plugins = ('pytest_asyncio', )
+
+
+@pytest.mark.asyncio
+async def test_get_states_swarco():
+    obj =  snmp_api.SwarcoStcip(ipv4='10.179.14.185', engine=SnmpEngine())
+    await obj.get_states()
+    # assert isinstance(obj.response_as_dict, abc.MutableMapping)
+    for field in ['data', 'errors']:
+        assert field in obj.response_as_dict
 
 
 
@@ -13,5 +27,5 @@ async def main():
 
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# if __name__ == '__main__':
+#     asyncio.run(main())
