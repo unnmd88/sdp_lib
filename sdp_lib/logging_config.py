af4de4e8c0a46906198e70de90b2d200764741c4
unnmd88 <unnmd88@gmail.com>
2025-05-16 23:08:29 +0300

    создан новый пакет для сервиса modbus, обновлен logging_config.py

--- sdp_lib/logging_config.py
+++ sdp_lib/logging_config.py
@@ -9,7 +9,13 @@ LOGGING_CONFIG = {
             "level": "DEBUG",
             "class": "logging.FileHandler",
             "filename": "log.log",
-            "formatter": "verbose",
+            "formatter": "verbose2",
+        },
+        "file2": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "reduce_log.log",
+            "formatter": "verbose2",
         },
         "console": {
             "level": "DEBUG",
@@ -23,11 +29,26 @@ LOGGING_CONFIG = {
             "handlers": ["console"],
             "propagate": True,
         },
+        "full_log": {
+            "level": "INFO",
+            "handlers": ['file'],
+            "propagate": True,
+        },
+        "reduce_log": {
+            "level": "INFO",
+            "handlers": ['file2'],
+            "propagate": True,
+        }
     },
     "formatters": {
         "verbose": {
             # "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
             "format": "{name} {levelname} {asctime} {module} {lineno} {funcName} {message} ",
+            "style": "{",
+        },
+        "verbose2": {
+            # "format": "{name} {levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "format": "{name} [{levelname:^11}] {asctime} {message}",
             "style": "{",
         },
         "simple": {
