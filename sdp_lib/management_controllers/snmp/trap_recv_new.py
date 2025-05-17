b9cf08484f050e46ccb9655d9d071b427fb43bb8
auser <unnmd88@gmail.com>
2025-05-18 00:46:29 +0300

    Начало реализации обработчиков трапов

--- sdp_lib/management_controllers/snmp/trap_recv_new.py
+++ sdp_lib/management_controllers/snmp/trap_recv_new.py
@@ -1,7 +1,9 @@
+import collections
+import sys
 from collections.abc import Sequence, Callable
 from enum import StrEnum
-from runpy import run_path
-from typing import TypeAlias
+from functools import cached_property
+from typing import TypeAlias, Any
 
 from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncio.dgram import udp
@@ -9,104 +11,63 @@ from pysnmp.entity.engine import SnmpEngine
 from pysnmp.entity.rfc3413 import ntfrcv
 import ipaddress
 
+
 from sdp_lib.utils_common.utils_common import check_is_ipv4
 
-# Create SNMP engine with autogenernated engineID and pre-bound
-# to socket transport dispatcher
-snmp_engine = engine.SnmpEngine()
-
-# Transport setup
-
-# UDP over IPv4, first listening interface/port
-# config.add_transport(
-#     snmp_engine,
-#     udp.DOMAIN_NAME + (1,),
-#     udp.UdpTransport().open_server_mode(("192.168.45.248", 164)),
-# )
-
-# UDP over IPv4, second listening interface/port
-# config.add_transport(
-#     snmpEngine,
-#     udp.DOMAIN_NAME + (2,),
-#     udp.UdpTransport().open_server_mode(("127.0.0.1", 2162)),
-# )
 
 T_Interfaces: TypeAlias = Sequence[tuple[str, int]]
 T_CommunityData: TypeAlias = Sequence[tuple[str, str]]
 
-def _cbFun(snmpEngine, stateReference, contextEngineId, contextName, varBinds, cbCtx):
+
+def handler1(varbinds, *args, **kwargs):
+    print(f'< Handler 1 >')
+    for name, val in varbinds:
+        print(f"{str(name)} = {str(val)}")
+    print(f'End handler 1')
+
+
+def handler2(varbinds, *args, **kwargs):
+    print(f'< Handler 2 >')
+    for name, val in varbinds:
+        print(f"{str(name)} = {str(val)}")
+    print(f'End handler 2')
+
+
+class HandlersData:
+    def __init__(self):
+        self._handlers = {}
+        self._max_handlers = 10
+
+    def register_handlers(self, *args: tuple[str, Callable]):
+        for ip, handler in args:
+            if not callable(handler):
+                raise ValueError(f'Обработчик должен быть вызываемым объектом')
+            ipaddress.IPv4Address(ip)
+            if ip not in self._handlers:
+                self._handlers[ip] = collections.deque(maxlen=self._max_handlers)
+            self._handlers[ip].append(handler)
+
+    def get_registered_handlers(self):
+        return self._handlers
+
+
+handlers = HandlersData()
+handlers.register_handlers(('10.45.154.11', handler1), ('10.45.154.11', handler2),
+                           ('10.45.154.12', handler1))
+
+
+def _cbFun(snmp_engine, stateReference, contextEngineId, contextName, varBinds, cbCtx):
     # Callback function for receiving notifications
     # noinspection PyUnusedLocal,PyUnusedLocal,PyUnusedLocal
-    # print(
-    #     'Notification from ContextEngineId "{}", ContextName "{}"'.format(
-    #         contextEngineId.prettyPrint(), contextName.prettyPrint()
-    #     )
-    # )
-    exec_context = snmpEngine.observer.get_execution_context(
-        'rfc3412.receiveMessage:request'
-    )
-    print(
-        f'Notification from {exec_context["transportAddress"]}, Domain {exec_context["transportDomain"]}'
-    )
-
-
-    # print(f'snmpEngine: {type(snmpEngine)}\n')
-    # print(f'snmpEngine cache: {snmpEngine.cache}\n'
-    #       f'execContext c: {execContext}\n',
-    #       f'stateReference: {type(stateReference)}\n'
-    #       f'contextEngineId: {type(contextEngineId)}\n'
-    #       f'contextName: {type(contextName)}\n'
-    #       f'cbCtx: {type(cbCtx)}')
-
-    for name, val in varBinds:
-        # print(f"{name.prettyPrint()} = {val.prettyPrint()}")
-        print(f"{str(name)} = {str(val)}")
-
-def add_transport(
-    snmp_engine: SnmpEngine,
-    net_interfaces: T_Interfaces
-):
-    for i, interface in enumerate(net_interfaces, 1):
-        ip_v4, port = interface
-        if not check_is_ipv4(ip_v4):
-            raise ValueError(f'Некорректно задан ip адрес: {ip_v4}')
-        elif not 0 < int(port) < 65535:
-            raise ValueError(f'Некорректно задан порт: {port}')
-        # UDP over IPv4, second listening interface/port
-        config.add_transport(
-            snmp_engine,
-            udp.DOMAIN_NAME + (i,),
-            udp.UdpTransport().open_server_mode((ip_v4, port)),
-        )
-
-def register_community(
-    snmp_engine: SnmpEngine,
-    community_data: T_CommunityData
-):
-    for community_index, community_name in community_data:
-        # SecurityName <-> CommunityName mapping
-        config.add_v1_system(snmp_engine, community_index, community_name)
-
-def register_receiver(
-    snmp_engine: SnmpEngine,
-    callback_func: Callable
-):
-    # Register SNMP Application at the SNMP engine
-    ntfrcv.NotificationReceiver(snmp_engine, _cbFun)
-
-def setup_receiver(
-    snmp_engine: SnmpEngine,
-    interfaces: T_Interfaces,
-    community_data: T_CommunityData,
-    callback_func: Callable
-):
-    add_transport(snmp_engine, interfaces)
-    register_community(snmp_engine, community_data)
-    register_receiver(snmp_engine, callback_func)
-
-
-class Handlers(StrEnum):
-    stage = 'stage'
+    exec_context = snmp_engine.observer.get_execution_context('rfc3412.receiveMessage:request')
+    source = exec_context["transportAddress"]
+    domain = exec_context["transportDomain"]
+    print(f'Notification from {source}, Domain {domain}')
+    for ip, handler in handlers.get_registered_handlers().items():
+        print(f'ip: {ip}\nhandler: {handler.__name__}')
+    # for name, val in varBinds:
+    #     # print(f"{name.prettyPrint()} = {val.prettyPrint()}")
+    #     print(f"{str(name)} = {str(val)}")
 
 
 class TrapReceiver:
@@ -115,13 +76,10 @@ class TrapReceiver:
             *,
             net_interfaces: T_Interfaces,
             community_data: T_CommunityData,
-            handlers: Sequence[Callable] = None
     ):
         self._snmp_engine = engine.SnmpEngine()
         self._net_interfaces = net_interfaces
         self._community_data = community_data
-        self._handlers = handlers
-
 
     def _add_transport_target(self):
         for i, interface in enumerate(self._net_interfaces, 1):
@@ -146,19 +104,10 @@ class TrapReceiver:
         # Register SNMP Application at the SNMP engine
         ntfrcv.NotificationReceiver(self._snmp_engine, _cbFun)
 
-    def _register_handlers(self):
-        if self._handlers is not None:
-            for handler in self._handlers:
-                try:
-                    Handlers(handler)
-                except ValueError:
-                    print(f'Обработчик должен быть из {Handlers}')
-
     def _setup(self):
         self._add_transport_target()
         self._register_community()
         self._register_receiver()
-        self._register_handlers()
 
     def run(self):
         self._setup()
@@ -171,27 +120,16 @@ class TrapReceiver:
         self._snmp_engine.close_dispatcher()
 
 
-# SNMPv1/2c setup
-
-# SecurityName <-> CommunityName mapping
-# config.add_v1_system(snmp_engine, "my-area", "public")
-
-
 if __name__ == '__main__':
+    server = TrapReceiver(
+        net_interfaces=[('192.168.45.248', 164)],
+        community_data=[("my-area", "public")]
+    )
     try:
-        # Create SNMP engine with autogenernated engineID and pre-bound
-        # to socket transport dispatcher
-        server = TrapReceiver(
-            net_interfaces=[('192.168.45.248', 164)],
-            community_data=[("my-area", "public")]
-        )
         server.run()
-        # snmp_engine = engine.SnmpEngine()
-        # setup_receiver()
-        # snmp_engine.transport_dispatcher.job_started(1)  # this job would never finish
-        # Run I/O dispatcher which would receive queries and send confirmations
-        # snmp_engine.open_dispatcher()
     except KeyboardInterrupt:
         print(f'Ctrl-C was pressed.')
     finally:
         server.shutdown()
+
+    # print(handlers.get_registered_handlers())
