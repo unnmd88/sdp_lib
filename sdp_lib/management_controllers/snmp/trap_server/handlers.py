b0706ba7116ed688f49aacc7a3b1742f5a5b5f91
auser <unnmd88@gmail.com>
2025-05-19 21:26:16 +0300

    FIX

--- sdp_lib/management_controllers/snmp/trap_server/handlers.py
+++ sdp_lib/management_controllers/snmp/trap_server/handlers.py
@@ -47,7 +47,7 @@ class AbstractHandler:
         self._max_stored_events = 1024
         self._event_storage = collections.deque(maxlen=self._max_stored_events)
         self._messages_storage = collections.deque(maxlen=self._max_stored_messages)
-        self.time_ticks_oid = self._get_time_ticks_oid()
+        # self.time_ticks_oid = self._get_time_ticks_oid()
         self._processed_varbinds: dict | None = None
 
     def __call__(
