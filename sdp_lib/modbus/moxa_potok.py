51db9327081782e6a55a663f7d2419a2caea6076
auser <unnmd88@gmail.com>
2025-06-05 20:33:26 +0300

    Продолжение реализации метода polling в модуле moxa_potok.py

--- sdp_lib/modbus/moxa_potok.py
+++ sdp_lib/modbus/moxa_potok.py
@@ -1,40 +1,148 @@
 import asyncio
 import time
+from collections import deque
+from collections.abc import Callable, MutableSequence, Iterable
 
 from sdp_lib.modbus.client import Modbus
-from sdp_lib.management_controllers.api import PotokS, snmp_engine
+from sdp_lib.management_controllers.api import PotokS, PotokP, PeekUg405, SwarcoStcip, snmp_engine
+
+
+
+class EventNames:
+    stage = 'stage'
+    cycle = 'cycle'
+
+
+
+class State:
+    def __init__(self, change_cyc_val):
+        self._current_val = None
+        self._prev_val = None
+        self._change_cyc_val = change_cyc_val
+        self._stage_counter: float = 0
+        self._cyc_counter: float = 0
+        # self._timestamp_point: float = 0
+        # self.check_if_state_has_changed: Callable = self._get_check_state_func()
+
+    # def _get_check_state_func(self) -> Callable:
+    #     if self._change_cyc_val is None:
+    #         return self._check_is_different_curr_prev
+    #     else:
+    #         return self._check_is_different_curr_expected_value
+
+    def __eq__(self, other):
+        if isinstance(other, State):
+            return self._current_val == other.current_state
+        return NotImplemented
+
+    def set_curr_val(self, val):
+        self._current_val = val
+
+    def set_prev_val(self, val):
+        self._prev_val = val
+    # def set_timestamp_point(self):
+    #     self._timestamp_point = time.perf_counter()
+
+    def get_stage_counter_val(self) -> float:
+        return time.perf_counter() - self._stage_counter
+
+    def get_cyc_counter_val(self) -> float:
+        return time.perf_counter() - self._cyc_counter
+
+    def restart_stage_counter(self):
+        self._stage_counter = time.perf_counter()
+
+    def restart_cyc_counter(self):
+        self._cyc_counter = time.perf_counter()
+
+    def process(self, val):
+        self._current_val = val
+        stg_cnt = self.get_stage_counter_val()
+        cyc_cnt = self.get_cyc_counter_val()
+
+        print(f'prev_val={self._prev_val}')
+        print(f'curr_val={self._current_val}')
+        print(f'Секунда цикла={cyc_cnt}')
+        print(f'Секунда цикла={cyc_cnt}')
+
+        if self._current_val != self._prev_val:
+            print(f'Время фазы {self._prev_val}={stg_cnt}')
+            self.restart_stage_counter()
+            if self._current_val == self._change_cyc_val:
+                print(f'Время цикла={cyc_cnt}')
+                self.restart_cyc_counter()
+            self._prev_val = self._current_val
+
+    @property
+    def current_state(self):
+        return self._current_val
+
+    # def _check_is_different_curr_prev(self) -> bool:
+    #     return self._current_val != self._prev_val
+    #
+    # def _check_is_different_curr_expected_value(self) -> bool:
+    #     return self._current_val != self._change_cyc_val
+
+
+
+
+class Logging:
+    def __init__(
+            self,
+            host: PotokS | SwarcoStcip | PotokP | PeekUg405 | Modbus,
+            state: State
+    ):
+        self._host = host
+        self._state = state
+
+
+
+
+
+
+
+async def logging_snmp(
+    snmp_instance: PotokS | SwarcoStcip | PotokP | PeekUg405,
+    state: State
+) -> MutableSequence[str]:
+    log_messages = deque(maxlen=8)
+
+
+
+
 
 async def polling(
     modbus_host: Modbus,
     snmp_host,
     delay: float
 ):
-    cycle_restart_stage = '1'
-    curr_stage_snmp = None
-    prev_stage_snmp = None
-    cyc_timer_modbus = time.perf_counter()
-    cyc_timer_snmp = time.perf_counter()
-    stage_timer_modbus = time.perf_counter()
-    stage_timer_snmp = time.perf_counter()
-
+    snmp_state = State(change_cyc_val='1')
 
     while True:
-        snmp_instance = await snmp_host.get_current_stage()
-        err = snmp_instance.response_errors
-        curr_stage_snmp = snmp_instance.response_data.get('current_stage')
-        curr_stage_snmp = str(curr_stage_snmp) if curr_stage_snmp is not None else curr_stage_snmp
-        if err:
-            print(err)
-        elif prev_stage_snmp is None: # Initial
-            prev_stage_snmp = snmp_instance
-        if not err and curr_stage_snmp != prev_stage_snmp and curr_stage_snmp == cycle_restart_stage:
-            print(f'Время цикла составило {time.perf_counter() - cyc_timer_snmp}')
-            cyc_timer_snmp = time.perf_counter()
+        await snmp_host.get_current_stage()
 
-        prev_stage_snmp = curr_stage_snmp
-        print(f'err: {err}')
-        print(f'stage: {curr_stage_snmp}')
-        print(f'Текущая секунда цикла: {int(time.perf_counter() - cyc_timer_snmp)}')
+        err_snmp = snmp_host.response_errors
+
+        curr_stage_snmp = snmp_host.response_data.get('current_stage')
+        curr_stage_snmp = str(curr_stage_snmp) if curr_stage_snmp is not None else curr_stage_snmp
+
+        if err_snmp:
+            print(err_snmp)
+            await asyncio.sleep(delay)
+            continue
+
+        elif curr_stage_snmp is None: # Initial
+            print('None в ответе фазы от ДК...')
+            await asyncio.sleep(delay)
+            continue
+
+        if snmp_state._prev_val is None:
+            snmp_state.set_prev_val(curr_stage_snmp)
+            await asyncio.sleep(delay)
+            continue
+
+        snmp_state.process(curr_stage_snmp)
+
         await asyncio.sleep(delay)
 
 
