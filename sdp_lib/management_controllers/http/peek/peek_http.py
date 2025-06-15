f0bd1c55d4a4a9c904e8043b1b1aefe727955931
unnmd88 <unnmd88@gmail.com>
2025-06-15 20:58:27 +0300

    Начало рефакторинга PeekWebHosts

--- sdp_lib/management_controllers/http/peek/peek_http.py
+++ sdp_lib/management_controllers/http/peek/peek_http.py
@@ -1,4 +1,6 @@
 import asyncio
+from collections.abc import Awaitable, MutableSequence
+from dataclasses import dataclass
 from enum import IntEnum
 from functools import cached_property
 from typing import (
@@ -29,12 +31,145 @@ from sdp_lib.management_controllers.structures import HttpResponseStructure
 
 T_Parsers = TypeVar('T_Parsers', MainPageParser, InputsPageParser)
 
+@dataclass
+class RequestConfig:
+    coros: MutableSequence[Awaitable] = None
+
+
+
 
 class DataFromWeb(IntEnum):
 
     main_page_get     = 1
     inputs_page_get   = 2
     inputs_page_set   = 3
+
+
+# class PeekWebHosts(HttpHosts):
+#
+#     @cached_property
+#     def matches(self) -> dict[DataFromWeb, tuple[str, Callable, Type[T_Parsers]]]:
+#         return {
+#             DataFromWeb.main_page_get: (routes.main_page, self._request_sender.fetch, PeekWebPagesParser),
+#             DataFromWeb.inputs_page_get: (routes.get_inputs, self._request_sender.fetch, InputsPageParser),
+#             DataFromWeb.inputs_page_set: (routes.set_inputs, self._request_sender.post_request, None),
+#         }
+#
+#     async def _single_common_request(
+#             self,
+#             url,
+#             method: Callable,
+#             parser_class,
+#             **kwargs
+#     ):
+#         self._tmp_response = await self._request_sender.http_request_to_host(
+#             url=url,
+#             method=method,
+#             **kwargs
+#         )
+#         if self.check_http_response_errors_and_add_to_host_data_if_has():
+#             return self
+#
+#         # print(f'self.last_response: {self.last_response}')
+#
+#         # if parser_class is None:
+#         #     # Вернуть ответ из self._request_sender.http_request_to_host если парсер не задан
+#         #     return self._tmp_response[HttpResponseStructure.CONTENT]
+#
+#         parser = PeekWebPagesParser()
+#         # parsed_data = parser.main_page_parser.parse()
+#
+#         # parser = parser_class()
+#         # parser.parse(self._tmp_response[HttpResponseStructure.CONTENT])
+#         # print(f'parser.data_for_response: {parser.data_for_response}')
+#
+#         self._response_storage.put_raw_responses(ResponseEntity(
+#             raw_data=self._tmp_response[HttpResponseStructure.CONTENT],
+#             name='PeekWeb',
+#             parser=parser.main_page_parser.parse
+#         ))
+#
+#         return self
+#
+#     """ Monitoring """
+#
+#     async def fetch_all_pages(self, *args, **kwargs):
+#         async with asyncio.TaskGroup() as tg:
+#             for page in args:
+#                 route, method, parser_class = self.matches.get(page)
+#                 tg.create_task(
+#                     self._single_common_request(
+#                         self._base_url + route, method, parser_class,
+#                         **kwargs
+#                     )
+#                 )
+#         # if self.response_errors:
+#         #     self.remove_data_from_response()
+#         return self
+#
+#     async def get_states(self):
+#         return await self.fetch_all_pages(DataFromWeb.main_page_get)
+#
+#     async def get_inputs(self):
+#         return await self.fetch_all_pages(DataFromWeb.inputs_page_get)
+#
+#     """ Management """
+#
+#     async def post_all_pages(self, page, payload_data: list[tuple]):
+#         async with asyncio.TaskGroup() as tg:
+#             results = []
+#             print(f'page: {page}\npayload: {payload_data}')
+#             route, method, parser_class = self.matches.get(page)
+#             max_concurrent_tasks = 5
+#             for num_task, payload in enumerate(payload_data, 1):
+#                 if num_task % max_concurrent_tasks == 0:
+#                     print(f'DEBUG len(payload_data) : {len(payload_data)}')
+#                     await asyncio.sleep(1) # Peek сбрасывает при коннект при большом количестве запросов
+#                 results.append(
+#                     tg.create_task(
+#                         self._single_common_request(
+#                             self._base_url + route, method, parser_class,
+#                             cookies=static_data.cookies,
+#                             data=payload
+#                         )
+#                     )
+#                 )
+#
+#         # print(f'results_results: + {results}')
+#         if self.response_errors:
+#             self.remove_data_from_response()
+#         return self
+#
+#     async def set_inputs_to_web(
+#             self,
+#             *,
+#             inps_name_and_vals: dict | tuple = None,
+#             stage: int = None,
+#     ):
+#         await self.get_inputs()
+#         if self.response_errors:
+#             return self
+#         _inputs = self.response_as_dict['data']['inputs']
+#
+#         if stage is not None:
+#             payloads = InputsVarbinds(_inputs).get_varbinds_set_stage(stage)
+#         else:
+#             payloads = InputsVarbinds(_inputs).get_varbinds_as_from_name(inps_name_and_vals)
+#
+#         print(f'payloads: {payloads}')
+#         await self.post_all_pages(
+#             DataFromWeb.inputs_page_set,
+#             payload_data=payloads
+#         )
+#         await self.get_inputs()
+#         return self
+#
+#     async def set_stage(self, stage: int):
+#
+#         if stage not in range(9):
+#             self.add_data_to_data_response_attrs(BadValueToSet(value=stage, expected=(0, 8)))
+#             return self
+#         return await self.set_inputs_to_web(stage=stage)
 
 
 class PeekWebHosts(HttpHosts):
@@ -162,7 +297,6 @@ class PeekWebHosts(HttpHosts):
             self.add_data_to_data_response_attrs(BadValueToSet(value=stage, expected=(0, 8)))
             return self
         return await self.set_inputs_to_web(stage=stage)
-
 
 
 """ Tests """
