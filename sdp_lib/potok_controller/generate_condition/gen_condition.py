3d95e17545de43b8bcd36f5fb83c82f2b039e67f
unnmd88 <unnmd88@gmail.com>
2025-07-14 14:25:49 +0300

    Переезд в модуль gen_condition.py

--- sdp_lib/potok_controller/generate_condition/gen_condition.py
+++ sdp_lib/potok_controller/generate_condition/gen_condition.py
@@ -1,191 +1,217 @@
 import re
-from collections import deque
-from dataclasses import dataclass
+import sys
+from collections.abc import MutableMapping
 from enum import StrEnum
-from typing import MutableSequence
+from typing import (
+    MutableSequence,
+    Any
+)
+
+
+ALLOWED_OPERATORS = {'|': 'or', '&': 'and'}
+
+
+def remove_spaces(string: str, left=True, right=True):
+    if left:
+        string = string.lstrip()
+    if right:
+        string = string.rstrip()
+    return string
+
+
+def get_main_and_mr_expr(
+    cond_string: str,
+    sep: str = ','
+) -> tuple[None | str, str, str]:
+    gr = cond_string.split(sep)
+    error = None
+    main_expr = mr_expr = ''
+    if len(gr) == 2:
+        try:
+            num_group = int(gr[1])
+            main_expr, mr_expr = gr[0], f'and mr(G{num_group})'
+        except ValueError:
+            error = f'Номер группы после "{sep}" должен быть числом.'
+    elif len(gr) == 1:
+        main_expr, mr_expr = gr[0], ''
+    else:
+        error = ErrMessages.invalid_condition
+    # print(f'>> {error, main_expr, mr_expr}')
+    return error, remove_spaces(main_expr), remove_spaces(mr_expr)
+
+
+class FieldNames(StrEnum):
+    errors = 'errors'
+    result_condition = 'result_condition'
 
 
 class ErrMessages(StrEnum):
     invalid_condition = 'Некорректное условие'
 
 
-def get_main_and_mr_expr(cond_string: str) -> tuple[None | str, str, str]:
-    gr = cond_string.split(',')
-    error = None
-    main_expr = mr_expr = ''
-    if len(gr) == 2:
-        main_expr, mr_expr = gr[0], f' and mr(G{gr[1]})'
-    elif len(gr) == 1:
-        main_expr, mr_expr = gr[0], ''
-    else:
-        error = ErrMessages.invalid_condition
-    print(f'>> {error, main_expr, mr_expr}')
-    return error, main_expr, mr_expr
-
-
-
-
-
-allowed_operators = {'|': 'or', '&': 'and'}
-
-
-class Cond:
-
-    def __init__(self, expr: str, left_op = '', func_name = 'ddr'):
-        self._raw_expr = expr
-        self._left_op = left_op
+class Tokens:
+    def __init__(self, token: str, func_name = 'ddr'):
+        self._raw_token = token.replace(" ", '')
         self._func_name = func_name
         self._errors = []
-        self._processed_expr = ''
-        for op in allowed_operators:
-            success_splited = self._raw_expr.split(op)
+        self._left = ''
+        self._right = ''
+        self._op = ''
+        self._processed_expr = None
+        self._check_token_and_add_err_if_has()
+
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}('
+            f'errors={self._errors} '
+            f'raw_token={self._raw_token} '
+            f'func_name="{self._func_name}" '
+            f'processed_expr="{self._processed_expr}"'
+            f')'
+        )
+
+    def _check_token_and_add_err_if_has(self):
+        self._processed_expr = ALLOWED_OPERATORS.get(self._raw_token, '')
+        if self._processed_expr:
+            return self._processed_expr
+
+        for op in ALLOWED_OPERATORS:
+            success_splited = self._raw_token.split(op)
             if len(success_splited) == 2:
-                try:
-                    self._left, self._right = map(int, success_splited)
-                except ValueError:
-                    self._errors.append(f'Ошибка в выражении: {op}. Номер группы должен быть числом')
-                self._op = op.replace(" ", '')
+                self._left, self._right = success_splited
+                self._op = ALLOWED_OPERATORS[op]
                 break
         else:
-            self._errors.append(f'Недопустимые символы в выражении {self._raw_expr}')
-
-        if left_op and left_op not in allowed_operators:
-            self._errors.append(f'Недопустимый оператор перед условием: {self._raw_expr}')
-        if not self._errors:
-            op = allowed_operators[self._op]
-            self._processed_expr = (
-                f' {op} '.join(f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
+            self._errors.append(
+                f'Отсутствует допустимый оператор в выражении "{self._raw_token}". '
+                f'Используйте один из: {set(f"{op}" for op in ALLOWED_OPERATORS)}'
             )
+            return False
+        try:
+            self._left, self._right = map(int, success_splited)
+        except ValueError:
+            self._errors.append(f'Ошибка в выражении: <{self._raw_token}>. Номер группы должен быть числом')
+            return False
 
-    def get_raw_expression(self):
-        return self._raw_expr
+        self._op = ALLOWED_OPERATORS[op]
+        self._processed_expr = (
+            f' {self._op} '.join(f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
+        )
+        return True
+
+    def _validate_token(self):
+        self._processed_expr = ALLOWED_OPERATORS.get(self._raw_token, '')
+        if not self._processed_expr:
+            for op in ALLOWED_OPERATORS:
+                success_splited = self._raw_token.split(op)
+                if len(success_splited) == 2:
+                    try:
+                        self._left, self._right = map(int, success_splited)
+                        self._op = ALLOWED_OPERATORS[op]
+                        self._processed_expr = (
+                            f' {self._op} '.join(
+                                f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
+                        )
+                    except ValueError:
+                        self._processed_expr = ''
+                        self._errors.append(f'Ошибка в выражении: <{self._raw_token}>. Номер группы должен быть числом')
+                    break
+            else:
+                self._errors.append(f'Недопустимые символы в выражении {self._raw_token}')
 
     def get_errors(self):
         return self._errors
 
-    def get_left_op(self):
-        return self._left_op
+    def get_raw_token(self):
+        return self._raw_token
 
     def get_func_name(self):
         return self._func_name
 
-    def get_processed_expression(self):
+    def get_processed_condition(self, wrap_parentheses: bool = False):
+        if wrap_parentheses and self._processed_expr not in ALLOWED_OPERATORS.values():
+            return f'({self._processed_expr})'
         return self._processed_expr
-
-    #
-    #
-    # def __repr__(self):
-    #     return (
-    #         f'{self.__class__.__name__}('
-    #         f'expr={self._expr} '
-    #         f'left_op={self._left_op} '
-    #         f'func_name={self._func_name} '
-    #         f'condition_string=<{self.get_string_condition()}>'
-    #         f')'
-    #     )
-    #
-    # def set_func_name(self, name: str):
-    #     self._func_name = name
-    #
-    # def get_string_condition(self):
-    #     op = allowed_operators[self._op]
-    #     cond = f' {op} '.join(f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
-    #     # print(f'cond: {cond}')
-    #     # print(f'{self._left_op} {cond}')
-    #     if self._left_op:
-    #         return f' {allowed_operators[self._left_op]} {cond}'
-    #     return cond
 
 
 class ConditionMaker:
     def __init__(self, raw_string: str):
-        self._raw_string = re.sub(r' {2,}', ' ', raw_string)
-        self._processed_string = ''
+        self._raw_string = re.sub(r' {2,}', ' ', raw_string).rstrip().lstrip()
+        self._result_expr = ''
         self._errors = []
         self._main_expr = self._and_mr_expr = ''
         self._tokens_to_parse = self._parsed_tokens = None
 
-    def get_result_as_dict(self):
-        print(self._parsed_tokens)
-        print(self._tokens_to_parse)
-        return {'errors': self._errors, 'processed_string': self._processed_string}
+    def __repr__(self):
+        return (
+            f'{self.__class__.__name__}('
+            f'raw_string="{self._raw_string}" '
+            f'errors={self._errors} '
+            f'tokens_to_parse={self._tokens_to_parse} '
+            f'parsed_tokens={self._parsed_tokens} '
+            f'main_expr="{self._main_expr}" '
+            f'and_mr_expr="{self._and_mr_expr}" '
+            f'result_expr="{self._result_expr}"'
+            f')'
+        )
 
-    def build_processed_condition_string(self):
+    def _create_parsed_tokens(self):
+        self._parsed_tokens = []
+        for raw_token in self._tokens_to_parse:
+            token = Tokens(raw_token)
+            self._errors += token.get_errors()
+            self._parsed_tokens.append(token)
+        return self._parsed_tokens
+
+    def _create_main_expr(self):
+        if len(self._tokens_to_parse) > 1:
+            return ' '.join(token.get_processed_condition(wrap_parentheses=True) for token in self._parsed_tokens)
+        return ' '.join(token.get_processed_condition() for token in self._parsed_tokens)
+
+    def _create_result_condition_string(self) -> str:
+        if not self._errors:
+            if self._and_mr_expr:
+                self._result_expr = f'({self._create_main_expr()}) {self._and_mr_expr}'
+            else:
+                self._result_expr = self._create_main_expr()
+        else:
+            self._result_expr = ''
+        return self._result_expr
+
+    def build_result_as_dict(self) -> MutableMapping[str, Any]:
+        return {FieldNames.errors: self._errors, FieldNames.result_condition: self._create_result_condition_string()}
+
+    def process_data_and_build_result_as_dict(self):
         err, self._main_expr, self._and_mr_expr = get_main_and_mr_expr(self._raw_string)
-        if err is not None:
+        if err is not  None:
             self._errors.append(err)
-            return
-        self._tokens_to_parse = self._main_expr.split()
-        stack_tokens_to_parse = deque(self._tokens_to_parse)
-        self._parsed_tokens: MutableSequence[Cond] = []
-        self._processed_string = ''
-        if len(self._tokens_to_parse) % 2 == 0 or self._tokens_to_parse[0] in allowed_operators:
-            self._errors.append(ErrMessages.invalid_condition)
-            return
-        first = Cond(stack_tokens_to_parse.popleft())
-        self._parsed_tokens.append(first)
-        while stack_tokens_to_parse:
-            left_op, expr = stack_tokens_to_parse.popleft(), stack_tokens_to_parse.popleft()
-            self._parsed_tokens.append(Cond(expr, left_op))
-        self.build_condition_string()
-
-    def build_condition_string(self) -> str:
-        for t in self._parsed_tokens:
-            self._processed_string += t.get_processed_condition()
-        if self._and_mr_expr:
-            self._processed_string = f'({self._processed_string}) {self._and_mr_expr}'
-        self._processed_string = re.sub(r' {2,}', ' ', self._processed_string)
-        print(f'self._new_condition_string: {self._processed_string}')
-        return self._processed_string
+        else:
+            self._tokens_to_parse = self._main_expr.split()
+            self._parsed_tokens: MutableSequence[Tokens] = []
+            if len(self._tokens_to_parse) % 2 == 0 or self._tokens_to_parse[0] in ALLOWED_OPERATORS:
+                self._errors.append(ErrMessages.invalid_condition)
+            else:
+                self._create_parsed_tokens()
+        return self.build_result_as_dict()
 
 
-
-def parse_string(string: str):
-    processed_condition = ConditionMaker(string)
-    processed_condition.build_processed_condition_string()
-    print(processed_condition.get_result_as_dict())
-
-
-
-# def parse_string(string: str) -> list[str]:
-#     string = re.sub(r' {2,}', ' ', string)
-#     parsed_tokens = []
-#     main_expr, mr_expr = get_main_and_mr_expr(string)
-#     print(f'main_expr: {main_expr}')
-#     print(f'mr_expr: {mr_expr}')
-#     print(f'main_expr.splited: {main_expr.split()}')
-#     tokens = deque(main_expr.split())
-#     print(f'tokens: {tokens}')
-#     if len(tokens) % 2 == 0 or tokens[0] in allowed_operators:
-#         raise ValueError
-#
-#     first = Cond(tokens.popleft())
-#     print(first)
-#     # first.get_string_condition()
-#     parsed_tokens.append(first)
-#     while tokens:
-#         left_op, expr = tokens.popleft(), tokens.popleft()
-#         parsed_tokens.append(Cond(expr, left_op))
-#     res = ''
-#     for r in parsed_tokens:
-#         r.get_string_condition()
-#         res += r.get_string_condition()
-#     if mr_expr:
-#         res = f'({res}) {mr_expr}'
-#     res = re.sub(r' {2,}', ' ', res)
-#     print(f'rrr: {res}')
-
-
+def debug():
+    while True:
+        try:
+            print()
+            user_data = input('Ввод выражения: ')
+            _maker = ConditionMaker(user_data)
+            print(_maker.process_data_and_build_result_as_dict())
+            print(_maker)
+        except KeyboardInterrupt:
+            print(f'\nЗавершаю выполнение...')
+            sys.exit()
 
 
 if __name__ == '__main__':
-    # generate_condition_string('1-3, 2-5,')
-    s = '1|3, 2-5,'.replace(" ", '')
-    pt = r'\d[\|,\&]{1}\d'
-    # print(re.findall(pt, s))
-    # print('3|8 & 5&9 | 10|17'.split())
-    s_str = '3|8 & 5&9 | 10|17,12'
-    print(parse_string(s_str))
-    # print()
-    # s_str = '1|2'
-    # print(parse_string(s_str))
+    # s_str = ' 5&9 | 10|17 | 121|124,12'
+    # s_str = ' 1|2 ,14'
+    # maker = ConditionMaker(s_str)
+    # print(maker.process_data_and_build_result_as_dict())
+    # print(maker)
+    debug()
