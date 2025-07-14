285bbee7adf4aaa7313cc94d2e6202c55599d889
unnmd88 <unnmd88@gmail.com>
2025-07-14 10:12:25 +0300

    Реализованы функции remove_spaces, debug. Добавлена проверка номера группы в функции get_main_and_mr_expr

--- sdp_lib/potok_controller/generate_condition/gen_condition2.py
+++ sdp_lib/potok_controller/generate_condition/gen_condition2.py
@@ -1,7 +1,43 @@
 import re
+import sys
 from collections.abc import MutableMapping
 from enum import StrEnum
-from typing import MutableSequence, Any
+from typing import (
+    MutableSequence,
+    Any
+)
+
+
+ALLOWED_OPERATORS = {'|': 'or', '&': 'and'}
+
+
+def remove_spaces(string: str, left=True, right=True):
+    if left:
+        string = string.lstrip()
+    if right:
+        string = string.rstrip()
+    return string
+
+
+def get_main_and_mr_expr(
+    cond_string: str,
+    sep: str = ','
+) -> tuple[None | str, str, str]:
+    gr = cond_string.split(sep)
+    error = None
+    main_expr = mr_expr = ''
+    if len(gr) == 2:
+        try:
+            num_group = int(gr[1])
+            main_expr, mr_expr = gr[0], f'and mr(G{num_group})'
+        except ValueError:
+            error = f'Номер группы после "{sep}" должен быть натуральным числом.'
+    elif len(gr) == 1:
+        main_expr, mr_expr = gr[0], ''
+    else:
+        error = ErrMessages.invalid_condition
+    # print(f'>> {error, main_expr, mr_expr}')
+    return error, remove_spaces(main_expr), remove_spaces(mr_expr)
 
 
 class FieldNames(StrEnum):
@@ -11,23 +47,6 @@ class FieldNames(StrEnum):
 
 class ErrMessages(StrEnum):
     invalid_condition = 'Некорректное условие'
-
-
-def get_main_and_mr_expr(cond_string: str) -> tuple[None | str, str, str]:
-    gr = cond_string.split(',')
-    error = None
-    main_expr = mr_expr = ''
-    if len(gr) == 2:
-        main_expr, mr_expr = gr[0], f'and mr(G{gr[1]})'
-    elif len(gr) == 1:
-        main_expr, mr_expr = gr[0], ''
-    else:
-        error = ErrMessages.invalid_condition
-    print(f'>> {error, main_expr, mr_expr}')
-    return error, main_expr, mr_expr
-
-
-allowed_operators = {'|': 'or', '&': 'and'}
 
 
 class Tokens:
@@ -46,26 +65,26 @@ class Tokens:
             f'{self.__class__.__name__}('
             f'errors={self._errors} '
             f'raw_token={self._raw_token} '
-            f'func_name={self._func_name} '
-            f'processed_expr=<{self._processed_expr}>'
+            f'func_name="{self._func_name}" '
+            f'processed_expr="{self._processed_expr}"'
             f')'
         )
 
     def _check_token_and_add_err_if_has(self):
-        self._processed_expr = allowed_operators.get(self._raw_token, '')
+        self._processed_expr = ALLOWED_OPERATORS.get(self._raw_token, '')
         if self._processed_expr:
             return self._processed_expr
 
-        for op in allowed_operators:
+        for op in ALLOWED_OPERATORS:
             success_splited = self._raw_token.split(op)
             if len(success_splited) == 2:
                 self._left, self._right = success_splited
-                self._op = allowed_operators[op]
+                self._op = ALLOWED_OPERATORS[op]
                 break
         else:
             self._errors.append(
-                f'Отсутствует допустимый оператор в выражении <{self._raw_token}>. '
-                f'Используйте один из: {set(f"{op}" for op in allowed_operators)}'
+                f'Отсутствует допустимый оператор в выражении "{self._raw_token}". '
+                f'Используйте один из: {set(f"{op}" for op in ALLOWED_OPERATORS)}'
             )
             return False
         try:
@@ -74,21 +93,21 @@ class Tokens:
             self._errors.append(f'Ошибка в выражении: <{self._raw_token}>. Номер группы должен быть числом')
             return False
 
-        self._op = allowed_operators[op]
+        self._op = ALLOWED_OPERATORS[op]
         self._processed_expr = (
             f' {self._op} '.join(f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
         )
         return True
 
     def _validate_token(self):
-        self._processed_expr = allowed_operators.get(self._raw_token, '')
+        self._processed_expr = ALLOWED_OPERATORS.get(self._raw_token, '')
         if not self._processed_expr:
-            for op in allowed_operators:
+            for op in ALLOWED_OPERATORS:
                 success_splited = self._raw_token.split(op)
                 if len(success_splited) == 2:
                     try:
                         self._left, self._right = map(int, success_splited)
-                        self._op = allowed_operators[op]
+                        self._op = ALLOWED_OPERATORS[op]
                         self._processed_expr = (
                             f' {self._op} '.join(
                                 f'{self._func_name}(D{num})' for num in range(self._left, self._right + 1))
@@ -110,8 +129,7 @@ class Tokens:
         return self._func_name
 
     def get_processed_condition(self, wrap_parentheses: bool = False):
-        print(self._processed_expr)
-        if wrap_parentheses and self._processed_expr not in allowed_operators.values():
+        if wrap_parentheses and self._processed_expr not in ALLOWED_OPERATORS.values():
             return f'({self._processed_expr})'
         return self._processed_expr
 
@@ -131,9 +149,9 @@ class ConditionMaker:
             f'errors={self._errors} '
             f'tokens_to_parse={self._tokens_to_parse} '
             f'parsed_tokens={self._parsed_tokens} '
-            f'main_expr={self._main_expr} '
-            f'and_mr_expr={self._and_mr_expr} '
-            f'result_expr={self._result_expr} '
+            f'main_expr="{self._main_expr}" '
+            f'and_mr_expr="{self._and_mr_expr}" '
+            f'result_expr="{self._result_expr}"'
             f')'
         )
 
@@ -170,28 +188,29 @@ class ConditionMaker:
         else:
             self._tokens_to_parse = self._main_expr.split()
             self._parsed_tokens: MutableSequence[Tokens] = []
-            if len(self._tokens_to_parse) % 2 == 0 or self._tokens_to_parse[0] in allowed_operators:
+            if len(self._tokens_to_parse) % 2 == 0 or self._tokens_to_parse[0] in ALLOWED_OPERATORS:
                 self._errors.append(ErrMessages.invalid_condition)
             else:
                 self._create_parsed_tokens()
         return self.build_result_as_dict()
 
 
+def debug():
+    while True:
+        try:
+            user_data = input('Ввод выражения: ')
+            _maker = ConditionMaker(user_data)
+            print(_maker.process_data_and_build_result_as_dict())
+            print(_maker)
+        except KeyboardInterrupt:
+            print(f'Завершаю выполнение...')
+            sys.exit()
+
+
 if __name__ == '__main__':
-    # generate_condition_string('1-3, 2-5,')
-    s = '1|3, 2-5,'.replace(" ", '')
-    pt = r'\d[\|,\&]{1}\d'
-    # print(re.findall(pt, s))
-    # print('3|8 & 5&9 | 10|17'.split())
-    # s_str = '3|r & 5&9 | 10|17,12'
-    s_str = '& 5&9 | 10|17,12'
-    s_str = ' 5&9 | 10|17 | 121|124,12'
-    s_str = ' 1|2 | 14-17'
-    maker = ConditionMaker(s_str)
-    print(maker.process_data_and_build_result_as_dict())
-    print(maker)
-
-
-    # print()
-    # s_str = '1|2'
-    # print(parse_string(s_str))
+    # s_str = ' 5&9 | 10|17 | 121|124,12'
+    # s_str = ' 1|2 ,14'
+    # maker = ConditionMaker(s_str)
+    # print(maker.process_data_and_build_result_as_dict())
+    # print(maker)
+    debug()
