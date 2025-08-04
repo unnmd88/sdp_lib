684544e7458f1b64febbc7db6432d9205e005a09
auser <unnmd88@gmail.com>
2025-08-05 00:59:18 +0300

    Удалены закомментированные классы

--- sdp_lib/passport/base.py
+++ sdp_lib/passport/base.py
@@ -2,10 +2,9 @@ import re
 from collections import Counter
 from dataclasses import dataclass, field
 from functools import cached_property
-from typing import NamedTuple, Type
-from abc import abstractmethod
+from typing import NamedTuple
 from collections.abc import (
-    MutableSequence,
+
     MutableMapping,
     Iterable
 )
@@ -25,64 +24,12 @@ from sdp_lib.passport.utils import make_int_or_float_collection, get_int_or_floa
 from sdp_lib.utils_common.utils_common import remove_chars
 
 
-# class ColumnValues:
-#     __slots__ = ('_col_name', '_init_val', '_default_val', '_value')
-#
-#     def __init__(
-#             self,
-#             col_name: ColNamesDirectionsTable | ColNamesTimeProgramsTable,
-#             init_val: Any,
-#             default_val: Any,
-#             value: Any
-#     ):
-#         self._col_name = col_name
-#         self._init_val = init_val
-#         self._default_val = default_val
-#         self._value = value
-#
-#     def __repr__(self):
-#         attrs = ' '.join(f'{attr}={getattr(self, attr)!r}' for attr in self.__slots__)
-#         return f'{self.__class__.__name__}({attrs})'
-#
-#     @property
-#     def col_name(self):
-#         return self._col_name
-#
-#     @property
-#     def init_val(self):
-#         return self._init_val
-#
-#     @property
-#     def default_val(self):
-#         return self._default_val
-#
-#     @property
-#     def value(self):
-#         return self._value
-#
-#     def set_value(self, value):
-#         self._value = value
-
-
 class ColumnData(NamedTuple):
     col_name: ColNamesDirectionsTable | ColNamesTimeProgramsTable | str
     init_val: Any
     default_val: Any
     value: Any
     is_valid: bool = True
-
-
-# class AbstractEntity:
-#     """ Абстрактный базовый класс элемента паспорта(строка, таблица и т.д.) """
-#
-#     def __init__(self):
-#         self._err_and_warn = MessageStorage()
-#
-#     def get_message_storage(self):
-#         return self._err_and_warn
-#
-#     def _get_common_val(self, init_val, name: str, default_val=None) -> ColumnData:
-#         return ColumnData(ColNamesDirectionsTable.stages, init_val, default_val, init_val or default_val)
 
 
 class AbstractRow:
@@ -103,71 +50,6 @@ class AbstractRow:
 
 
 T_Row = TypeVar('T_Row', bound=AbstractRow)
-
-
-# class AbstractTable(AbstractEntity):
-#     """ Абстрактный базовый класс таблицы паспорта. """
-#
-#     table_name: str = ''
-#     allowed_cnt_row_props: set
-#     row_class: Any
-#
-#     def __init__(self, income_data: str):
-#         super().__init__()
-#         self._raw_data = income_data
-#         self._income_data_errors = MessageStorage(StorageNames.income_data)
-#         self._check_raw_data()
-#         self._rows: MutableMapping[float, T_Row] = {}
-#         self._rows_with_errors: MutableMapping[float, T_Row] = {}
-#         # self._max_direction_num: float = .0
-#         # self._max_stage: float = .0
-#
-#
-#     @abstractmethod
-#     def build(self):
-#         """ Основной метод создания данных для таблицы. """
-#         ...
-#
-#     def _check_raw_data(self) -> bool:
-#         """
-#         Проверяет валидность атрибута self._income_data
-#         :return: True если входные данные валидны для обработки, иначе False.
-#         """
-#         if len(self._raw_data) < 4:
-#             self._income_data_errors.add_errors(
-#                 Message(f'Некорректные данные для обработки и формирования таблицы {self.table_name}')
-#             )
-#         return self.income_data_is_valid
-#
-#     def _load_row(self, *args: tuple[float, T_Row]) -> int:
-#         """
-#          Добавляет пару ключ-значение в атрибут self._rows.
-#         :param args: Каждый элемент args - кортеж из 2 элементов, у которого 0 элемент - ключ, а 1 - значение.
-#         :return: Количество добавленных пар в self._rows
-#         """
-#         return add_record(self._rows, args)
-#
-#     def _load_row_with_err(self, *args: tuple[float, T_Row]):
-#         """
-#          Добавляет пару ключ-значение в атрибут self._rows_with_errors.
-#         :param args: Каждый элемент args - кортеж из 2 элементов, у которого 0 элемент - ключ, а 1 - значение.
-#         :return: Количество добавленных пар в self._rows_with_errors
-#         """
-#         return add_record(self._rows_with_errors, args)
-#
-#     def get_income_data(self):
-#         """ Возвращает входные данные. """
-#         return self._raw_data
-#
-#     @property
-#     def income_data_is_valid(self) -> bool:
-#         return not self._income_data_errors.errors
-#
-#     def get_rows_with_errors(self) -> MutableMapping[float, T_Row]:
-#         return self._rows_with_errors
-#
-#     def get_all_rows(self) -> MutableMapping[float, T_Row]:
-#         return self._rows
 
 
 class AbstractTable:
