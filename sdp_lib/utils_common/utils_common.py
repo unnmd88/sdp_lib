e070898c9659babba6aa856135a7b4d85d64ca76
auser <unnmd88@gmail.com>
2025-09-02 01:04:17 +0300

    Добавлена функция get_int_or_float в utils_common.py

--- sdp_lib/utils_common/utils_common.py
+++ sdp_lib/utils_common/utils_common.py
@@ -163,6 +163,51 @@ def get_max_or_default_if_target_is_empty(target: Iterable, returned_default_val
     return max(target, default=returned_default_val)
 
 
+def get_int_or_float(val: str) -> int | float | None:
+    """
+    Основная функция, содержащая логику определения валидности номера фазы или направления.
+    На вход подается строка с номером, который вернёт int или float, если номер валидный,
+    иначе вернёт None.
+    Допустимыми считаются следующие типы номеров: целые числа или числа через точку,
+    где после точки стоит единстенная цифра от 1 до 9.
+    Примеры допустимых номеоров: "1", "4", "26", "1.2", "5.1", "32.4" и т.д.
+    Превращает объект val в тип int | float | None.
+    :param val: Объект строки из которого будет получен объект int | float | None.
+    :return: Если строка val является целым числом, возвращает int(val).
+             Если строка val является числом с точкой, у которого после точки стоит
+             одна единственная целая цифра от 1 до 9, функция вернёт float(val).
+             Иначе возвращает None.
+
+    Примеры
+    --------
+    # >>> get_int_or_float("1")
+    # 1
+    # >>> get_int_or_float("2.1")
+    # 2.1
+    # >>> get_int_or_float("3.2")
+    # 3.2
+    # >>> get_int_or_float("4.45")
+    # None
+    # >>> get_int_or_float("abracadabra")
+    # None
+
+    """
+    if isinstance(val, (int, float)):
+        return val
+    if not isinstance(val, str):
+        return None
+    if val.isdigit():
+        return int(val)
+    else:
+        assumption_is_float = val.split('.')
+        if len(assumption_is_float) != 2:
+            return None
+        before_dot, after_dot = assumption_is_float
+        if len(after_dot) != 1 or not after_dot.isdigit() or not before_dot.isdigit():
+            return None
+        return float(val)
+
+
 def add_record(
     target: MutableSequence | MutableMapping,
     records: Iterable[Any] | Iterable[tuple[Hashable, Any]],
